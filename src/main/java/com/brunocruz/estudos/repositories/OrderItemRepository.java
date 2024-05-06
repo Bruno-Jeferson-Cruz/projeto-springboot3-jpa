1287db5059ae795e330bd3ccc88c897c9f86525e
Bruno-Jeferson-Cruz <bjefersoncruz@gmail.com>
2024-05-06 12:37:36 -0300

    Product-OrderItem

--- src/main/java/com/brunocruz/estudos/repositories/OrderItemRepository.java
+++ src/main/java/com/brunocruz/estudos/repositories/OrderItemRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brunocruz.estudos.entities.OrderItem;
 
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 	
 }
