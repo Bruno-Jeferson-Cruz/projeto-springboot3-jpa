68bc0c21e0f37917f6d05cbae96e59036fbf818f
Bruno-Jeferson-Cruz <bjefersoncruz@gmail.com>
2024-05-06 12:49:18 -0300

    Payment

--- src/main/java/com/brunocruz/estudos/config/TesteConfig.java
+++ src/main/java/com/brunocruz/estudos/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.brunocruz.estudos.entities.Category;
 import com.brunocruz.estudos.entities.Order;
 import com.brunocruz.estudos.entities.OrderItem;
+import com.brunocruz.estudos.entities.Payment;
 import com.brunocruz.estudos.entities.Product;
 import com.brunocruz.estudos.entities.User;
 import com.brunocruz.estudos.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TesteConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepositoy.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 	
--- src/main/java/com/brunocruz/estudos/entities/Order.java
+++ src/main/java/com/brunocruz/estudos/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.brunocruz.estudos.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,12 +35,13 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	
-	@OneToMany(mappedBy="id.order")
-	private Set<OrderItem> items=new HashSet<>(); 
-	
-	
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
 	public Order() {
 		super();
 	}
@@ -84,9 +87,11 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	public Set<OrderItem> getItems(){
+
+	public Set<OrderItem> getItems() {
 		return items;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -110,5 +115,13 @@ public class Order implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 }
