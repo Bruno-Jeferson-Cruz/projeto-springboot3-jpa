a1b91faa64fc3808564b0ba728c39f9fc1eda651
Bruno-Jeferson-Cruz <bjefersoncruz@gmail.com>
2024-05-06 10:56:24 -0300

    Product add

---
+++ src/main/java/com/brunocruz/estudos/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.brunocruz.estudos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunocruz.estudos.entities.Product;
+import com.brunocruz.estudos.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
